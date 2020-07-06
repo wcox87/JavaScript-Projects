17494937fcb2b3daa704c50c28060936cc4030e1
Will Cox <wcox87@Gmail.com>
2020-07-06 00:02:13 -0600

    Logic Operators
    Add logic operators assignment

---
+++ Project 06/NaN.js
@@ -0,0 +1,51 @@
+//Typeof
+document.write(typeof "Batman" + "<br>");
+
+//Add an integer to a string
+document.write("100" + 5 + "<br>");
+
+//== True
+document.write(100 == 100);
+document.write("<br>")
+
+//== False
+document.write(10 == 1000);
+document.write("<br>")
+
+//=== True
+X = 100;
+Y = 100;
+document.write(X === Y);
+document.write("<br>")
+
+//=== False
+A = "Iron Man";
+B = "Captain America"
+document.write(A === B);
+document.write("<br>")
+
+//&& True
+document.write(10 > 3 && 3 < 5);
+document.write("<br>")
+
+//&& False
+document.write(100 < 50 && 45 > 30);
+document.write("<br>")
+
+//|| True
+document.write(20 < 5 || 5 > 1);
+document.write("<br>")
+
+//|| False
+document.write(100 < 5 || 45 > 100);
+document.write("<br>")
+
+//! False
+var notoperator = !(50 > 5);
+document.write(notoperator);
+document.write("<br>")
+
+//! True
+var notoperator2 = !(10 > 100);
+document.write(notoperator2);
+document.write("<br>")
